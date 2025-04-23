c9f48654d631443f8b2af82d76a1ecfaa63a2739
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2025-04-23 19:07:44 +0100

    Addition of change so namespace changes update kubectl hint

--- KubePilot/Extensions/StringExtensions.cs
+++ KubePilot/Extensions/StringExtensions.cs
@@ -1,9 +1,10 @@
-﻿namespace KubePilot.Extensions;
+﻿
+namespace KubePilot.Extensions;
 
 public static class StringExtensions
 {
     public static bool HasValue(this string? str) =>
         !string.IsNullOrWhiteSpace(str);
-    public static string NamespaceParameter(this string ns) =>
+    public static string NamespaceParameter(this string? ns) =>
             string.IsNullOrWhiteSpace(ns) ? "--all-namespaces" : $"-n {ns}";
 }
